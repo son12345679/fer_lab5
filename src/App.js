9dddf5d8114acfba822b0305c79632bdf2e04cbf
son <abcgmail.com>
2022-06-15 14:11:38 +0700

    up

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React, {Component} from "react";
+import DishDetail from "./components/DishDetailComponent.js";
+import { DISHES } from './shared/dishes.js';
+import { Navbar, NavbarBrand } from 'reactstrap';
+
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      dishes: DISHES
+  };
+}
+
+  render() {
+    return (
+      <div className="App">
+        <Navbar dark color="primary">
+          <div className="container">
+            <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
+          </div>
+        </Navbar>
+        <DishDetail dishes={this.state.dishes} />
+      </div>
+    );
+  }
+}
+
+
+export default App;
